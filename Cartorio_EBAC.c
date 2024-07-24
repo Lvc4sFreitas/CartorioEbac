879badcd177946f00b6361c0c313334977fd6a49
Lvc4sFreitas <llucas.br12@gmail.com>
2024-07-24 19:12:07 -0300

    Add files via upload

--- Cartorio_EBAC.c
+++ Cartorio_EBAC.c
@@ -112,7 +112,8 @@ int main()
   		printf("Escolha a opção desejada do menu\n\n");
   		printf("\t1 - Registrar nomes\n");
   		printf("\t2 - Consultar nomes\n");
-  		printf("\t3 - Deletar nomes\n\n");
+  		printf("\t3 - Deletar nomes\n");
+  		printf("\t4 - Sair do sistema\n\n");
   		printf("Opção: ");//Fim do menu
   	
   		scanf("%d", &opcao);//Armazenamento de escolha do usuario
@@ -133,6 +134,11 @@ int main()
   			case 3:
   			deletar();
   			break;
+  			
+  			case 4:
+            printf("Obrigado por utilizar nosso sistema!\n");
+            return 0;
+            break;
   			
   			default:
   			printf("Essa opção não está disponível!\n");
